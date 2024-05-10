3c95fb31bf9339f06b7c26955947d69d661ee0ec
luis <luis@luiss-MacBook-Air.local>
2024-05-09 23:49:13 -0300

    add the sring security

---
+++ src/main/java/br/ufrn/imd/helloworld/entity/Users.java
@@ -0,0 +1,26 @@
+package br.ufrn.imd.helloworld.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+}
